19f4186b44ccf9eb0472939a3ea60eba2633a8ac
IgorLopLego <igor.lopatchin@lego.com>
2022-11-03 15:27:41 +0100

    Scanner has been created

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
+import scanner.Scanner;
 import scanner.Source;
+import scanner.Token;
+import scanner.TokenKind;
 
 import javax.swing.*;
 
@@ -6,12 +9,16 @@ public class Main {
 
     public static void main(String[] args) {
         String projectPath =  System.getProperty("user.dir")+"/examples";
+        System.out.println("Path: " + projectPath);
         JFileChooser fileChooser = new JFileChooser(projectPath);
         fileChooser.showOpenDialog( null );
         Source source = new Source(fileChooser.getSelectedFile().getAbsolutePath());
-        char character = ' ';
-        while (character != '\0'){
-            character = source.getSource();
+        Scanner scanner = new Scanner(source);
+        Token token = scanner.scan();
+        while (token.getKind() != TokenKind.NULLTERMINANT)
+        {
+            System.out.println( token.getKind() + " " + token.getSpelling());
+            token = scanner.scan();
         }
 
     }
---
+++ src/scanner/Token.java
@@ -0,0 +1,84 @@
+package scanner;
+
+import static scanner.TokenKind.OPERATOR;
+
+public class Token {
+    private TokenKind kind;
+    private String spelling;
+    private static final String assignOperator = "#";
+    private static final String additionOperator[] = {"add", "subtract"};
+    private static final String multiplicationOperators[] = {"multiplication", "division", "module"};
+    private static final TokenKind[] KEYWORDS = {TokenKind.START, TokenKind.END, TokenKind.FOR, TokenKind.SWITCH, TokenKind.TERNARY, TokenKind.EXEFUNC, TokenKind.SCREAM, TokenKind.FOLLOWING};
+
+
+    public Token(TokenKind kind, String spelling)
+    {
+        this.kind = kind;
+        this.spelling = spelling;
+
+        if (kind == TokenKind.IDENTIFIER) {
+            if(IsOperator())
+            {
+                this.kind = OPERATOR;
+                return;
+            }
+            TokenKind tokenKind = IsAKeyword();
+            if(tokenKind != null)
+            {
+                this.kind = tokenKind;
+            }
+        }
+    }
+
+    private boolean IsOperator() {
+         return assignOperator.equals(spelling) || containsOperator(spelling, additionOperator) || containsOperator(spelling, multiplicationOperators);
+    }
+
+    private TokenKind IsAKeyword(){
+        for (TokenKind tokenKind : KEYWORDS) {
+            if (spelling.equals(tokenKind.getSpelling())) {
+               return tokenKind;
+            }
+        }
+        return null;
+    }
+
+    public boolean isAssignOperator() {
+        if (kind == OPERATOR) {
+            return spelling.equals(assignOperator);
+        }
+        return false;
+    }
+
+    public boolean isAddOperator()
+    {
+        if( kind == OPERATOR )
+            return containsOperator( spelling, additionOperator);
+        else
+            return false;
+    }
+
+    private boolean containsOperator( String spelling, String operations[] ) {
+
+        for (String operation : operations)
+            if (spelling.equals(operation))
+                return true;
+        return false;
+    }
+
+    public TokenKind getKind() {
+        return kind;
+    }
+
+    public String getSpelling() {
+        return spelling;
+    }
+
+
+
+
+
+
+
+
+}
